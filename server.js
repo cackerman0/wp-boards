816df16f5067a9252caaae1b091e892275ee664a
cackerman <coleack1@gmail.com>
2025-08-08 15:50:05 -0400

    added get /

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+app.get('/', (req, res) => {
+    console.log('endpoint found!')
+})
+
+
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`))
